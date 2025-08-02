840b0c5ac6ed596418e9c9b7345366c9316d7e89
Miles Tsue <65318978+Kyle2Miles93@users.noreply.github.com>
2025-08-02 18:53:21 -0400

    added template files

--- expense_tracker/expenses/views.py
+++ expense_tracker/expenses/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Expense
+from django import forms
 
-# Create your views here.
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ['title', 'amount', 'date', 'description']
+
+
+def expense_list(request):
+    expenses = Expense.objects.all()
+    return render(request, 'expenses/expense_list.html', {'expenses': expenses})
+
+
+def expense_create(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm()
+    return render(request, 'expenses/expense_form.html', {'form': form})
+
+
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('expense_list')
+    else:
+        form = ExpenseForm()
+    return render(request, 'expenses/add_expense.html', {'form': form})
